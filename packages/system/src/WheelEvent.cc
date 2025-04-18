e543c95b580e743163751691b766553ed88ab1e1
w30029682 <wangjinfan1@huawei.com>
2025-04-18 17:53:23 +0800

    #fix terminal

--- packages/system/src/WheelEvent.cc
+++
@@ -1,8 +0,0 @@
-#include "system/include/WheelEvent.hpp"
-#include "core/include/EventBase.hpp"
-using namespace aleph;
-using namespace aleph::system;
-WheelEvent::WheelEvent(bool direction)
-    : core::EventBase("wheel"), _direction(direction) {}
-
-bool WheelEvent::getDirection() const { return _direction; }
--- projects/darkland/src/launch.cc
+++ projects/darkland/src/launch.cc
@@ -1,7 +1,23 @@
-#include "runtime/include/Application.hpp"
-
-using namespace aleph;
+#include <clocale>
+#include <conio.h>
+#include <cstdio>
+#include <windows.h>
 int main(int argc, char *argv[]) {
-  runtime::Application app;
-  return app.run(argc, argv);
+  setlocale(LC_ALL, "");
+  HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD mode = 0;
+  GetConsoleMode(hOutput, &mode);
+  mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(hOutput, mode);
+  printf("\x1b[s");
+  printf("\x1b[?25l");
+  printf("\x1b[2J");
+  printf("\x1b[1;1H");
+  wprintf(L"key:中文输出");
+  printf("\x1b[2;1H");
+  wprintf(L"key:中文输出2");
+  _getch();
+  printf("\x1b[?25h");
+  printf("\x1b[u");
+  return 0;
 }
